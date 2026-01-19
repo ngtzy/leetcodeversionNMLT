2cfa4ada1c98c9352ccf9289265dfa1ac4f42c53
thuy vy <nguyenthuyvyvta2016@gmail.com>
2026-01-19 18:02:00 +0700

    Create twosumOOP.cpp
    version OOP

---
+++ twosumOOP.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int k = i + 1; k < nums.size(); k++) {
+                if (nums[i] + nums[k] == target) {
+                    return {i, k};
+                }
+            }
+        }
+        return {};
+    }
+};
