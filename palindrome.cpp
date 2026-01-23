b3ebf388b6a3c21a47f431f590f1abc638097ca3
thuy vy <nguyenthuyvyvta2016@gmail.com>
2026-01-23 18:32:01 +0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
 using namespace std;
-
-//could you solve it without converting the integer to a string?
-
-
+// string banned
 int reverseInteger(int a) {
 	int rev = 0;
-	
 	while (a != 0) {
-	int digit = a % 10;
-	rev = rev * 10 + digit;
-	a /= 10;
+		int digit = a % 10;
+		rev = rev * 10 + digit;
+		a /= 10;
 	}
-
 	return rev;
 }
-
 int main () {
 	int x;
 	cin >> x;
 	int k = x;
-	if (x < 0) {
-		cout << "false";
-	} 
-	else if (reverseInteger(x) == k) {
-		cout << "true";
-	}
+	if (x < 0) cout << "false";
+	else if (reverseInteger(x) == k) cout << "true";
 	else cout << "false";
 	return 0;
 }
