aeed324897eeb430c4e354b68d69d6eb59c98cbc
thuy vy <nguyenthuyvyvta2016@gmail.com>
2026-01-23 18:30:21 +0700

    Update romantointeger.cpp

--- romantointeger.cpp
+++ romantointeger.cpp
@@ -5,42 +5,28 @@ int main() {
     string s;
     cin >> s;
     int integer = 0;
-
     for (int i = 0; i<s.length(); i++) {
             switch (find) {
-                case 'I':
-                    integer += 1;
+                case 'I': integer += 1;
                 break;
-
-                case 'V':
-                    integer += 5;
+                case 'V': integer += 5;
                 break;
-
-                case 'X':
-                    integer += 10;
+                case 'X': integer += 10;
                 break;
-
-                case 'L':
-                    integer += 50;
+                case 'L': integer += 50;
                 break;
-
-                case 'C':
-                    integer += 100;
+                case 'C': integer += 100;
                 break;
-
-                case 'D':
-                    integer += 500;
+                case 'D': integer += 500;
                 break;
-
-                case 'M':
-                    integer += 1000;
+                case 'M': integer += 1000;
                 break;
             }
             if (s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) integer -=2;
             if (s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) integer -=20;
             if (s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M')) integer -=200;
-
       }
     cout << integer << endl;
     return 0;
 }
+
